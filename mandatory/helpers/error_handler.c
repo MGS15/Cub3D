282dd282770acd40d0639933c0111744186de6ea
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-03-31 21:40:39 +0000

    <add>: free_maze & double_pointer_len

--- mandatory/helpers/error_handler.c
+++ mandatory/helpers/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 02:25:16 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/03/31 00:43:09 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/03/31 20:54:31 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,36 @@ void	print_error(char *err_msg)
 {
 	printf("%sError%s%s: %s%s\n", BOLD_RED_COLOR,
 		DEFAULT_COLOR, RED_COLOR, err_msg, DEFAULT_COLOR);
+}
+
+void	free_double_pointer(void **pointer)
+{
+	int	i;
+
+	if (!pointer)
+		return ;
+	i = -1;
+	while (pointer[++i])
+		free(pointer[i]);
+	free(pointer);
+	pointer = NULL;
+}
+
+void	free_maze(t_maze *maze)
+{
+	if (!maze)
+		return ;
+	if (maze->north_side)
+		free(maze->north_side);
+	if (maze->south_side)
+		free(maze->south_side);
+	if (maze->west_side)
+		free(maze->west_side);
+	if (maze->east_side)
+		free(maze->east_side);
+	if (maze->floor)
+		free(maze->floor);
+	if (maze->ceilling)
+		free(maze->ceilling);
+	free(maze);
 }
--- mandatory/helpers/string_handler.c
+++ mandatory/helpers/string_handler.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 05:28:16 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/03/31 00:45:31 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/03/31 19:58:21 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,29 @@ bool	extention_checker(char *dir, const char *ext)
 	if (!ft_strncmp(tmp_ext, ext, ft_strlen(tmp_ext)))
 		return (true);
 	return (false);
+}
+
+size_t	double_pointer_len(void **pointer)
+{
+	int	i;
+
+	if (!pointer)
+		return (0);
+	i = -1;
+	while (pointer[++i])
+		;
+	return (i);
+}
+
+bool	ft_isnumeric(const char *s)
+{
+	if (!s)
+		return (false);
+	while (*s)
+	{
+		if (!ft_isdigit(*s))
+			return (false);
+		s++;
+	}
+	return (true);
 }
