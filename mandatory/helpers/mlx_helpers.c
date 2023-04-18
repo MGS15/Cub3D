51eb07cf244c69418fbda1f436fa87763c811ba1
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-04-18 00:27:30 +0000

    <rm>: useless function

--- mandatory/helpers/mlx_helpers.c
+++ mandatory/helpers/mlx_helpers.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 00:05:03 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/04/08 21:40:30 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/04/17 21:52:16 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,4 @@ void	my_mlx_pixle_put(t_img *img, t_coords coord, int color)
 
 	dst = img->addr + (coord.y * img->line_len + coord.x * (img->bpp / 8));
 	*(unsigned int *) dst = color;
-}
-
-long	square_of_number(int num)
-{
-	return ((long) (num * num));
 }
